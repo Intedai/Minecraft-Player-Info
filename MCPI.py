128607294dbb6e57222b6088327e4fd5e604d037
Ariel Deri <69306633+Intedai@users.noreply.github.com>
2022-10-31 15:44:31 +0200

    Cleaner file
    Edited the comments so they will be easier to understand them.

--- MCPI.py
+++ MCPI.py
@@ -1,32 +1,26 @@
 #This file is released under the terms of the MIT license.
 
 #imports
-import tkinter as tk #GUI
+import tkinter as tk
 import tkinter.filedialog as filedialog #for the save as file dialogue
 import requests #to get requests from the Mojang API
-import json #to load the JSON from the requests
-import base64 #decode the texture values from the request
+import json
+import base64
 from urllib.request import urlopen #to open the images
 from PIL import Image, ImageTk, ImageDraw #to display images, get images from the link and to make the error image
-from time import sleep #uses the sleep function for the error screen 
+from time import sleep
 from pyperclip import copy #uses the copy function to add the UUID to the clipboard
 #------
-
-
-
 
 #The entire software: the GUI and displaying everything from the PlayerProperties
 class Software():
     def __init__(self):
-
-        #creates the gui
         self.root = tk.Tk()
 
-        #size of the window and background color
         self.root.geometry("1000x525")
         self.root.config(bg = "#00838F")
 
-        #tries to sets the icon at the top left to icon.ico
+        #if icon.ico exists it will set it as the gui's icon
         try:
             self.root.iconbitmap("icon.ico")
         except:
@@ -35,41 +29,33 @@ class Software():
         #sets minimum size to 1000x525
         self.root.minsize(1000, 525)
 
-        #top bar
         self.user_label = tk.Label(self.root,text="MC Player Information", font=("Calibri",30,"bold"), fg="white" ,bg="#006064", width=1000)
         self.user_label.pack(pady=0)
 
-        #"username:" text
         self.username_text = tk.Label(self.root,text="Username:", font=("Calibri",15,"bold"), bg="#00838F", fg="white")
         self.username_text.pack(pady=5)    
 
-        #entry (text box that is only 1 line)
         self.user_entry = tk.Entry(self.root, width=60, font=("Calibri",18),fg="white", bg="#26C6DA", border=0)
         self.user_entry.pack()
 
-        #FIND INFO button, executes getPlayerProperties() when clicked
         self.info_button = tk.Button(self.root, height=1, width=10,text="FIND INFO", font=("Calibri",14), command=lambda:self.getPlayerProperties(),fg="white", bg="#006064", border=0, activebackground='#007782', activeforeground='white')
         self.info_button.pack(pady=20)
 
         #creates a playerProperties object to display when opening the softwarez before checking other usernames
         first_skin = PlayerProperties("Technoblade")
 
-        #"UUID:" text
         self.uuid_text = tk.Label(self.root,text="UUID:", font=("Calibri",15,"bold"), bg="#00838F", fg="white")
         self.uuid_text.pack()
 
-        #displays the UUID
         self.uuid = tk.Label(self.root,text=first_skin.playerId, font=("Calibri",13), bg="#00838F", fg="white")
         self.uuid.pack()
 
         #the UUID saved so the user will be able to copy it using the copy button, value changes everytime the user is checking another player.
         self.uuid_val = first_skin.playerId
 
-        #copy button
         self.copy_bttn = tk.Button(self.root, height=1, width=10,text="COPY", font=("Calibri",14), command=lambda:self.copy_UUID(),fg="white", bg="#006064", border=0, activebackground='#007782', activeforeground='white')
         self.copy_bttn.pack(pady=20)        
 
-        #"Skin:" text
         self.skin_text = tk.Label(self.root,text="Skin:", font=("Calibri",15,"bold"), bg="#00838F", fg="white")
         self.skin_text.pack()
 
@@ -91,11 +77,9 @@ class Software():
         ImageDraw.Draw(self.error_image).text((14, 11),'PLAYER\nNOT\nFOUND',(255,255,255))
         self.tk_error_image = ImageTk.PhotoImage(self.error_image)
 
-        #download button, executes self.save_skin()
         self.download_bttn = tk.Button(self.root, height=1, width=10,text="DOWNLOAD", font=("Calibri",14), command=lambda:self.save_skin(),fg="white", bg="#006064", border=0, activebackground='#007782', activeforeground='white')
         self.download_bttn.pack(pady=20)
 
-        #############
         tk.mainloop()
 
 
@@ -124,10 +108,6 @@ class Software():
             self.uuid.configure(text=newSkin.playerId)
             self.uuid.pack()
 
-            #sets the image
-            #self.skinPNG.image = self.tk_img
-            
-            #binds all of the changes
             self.root.bind("<Return>")
 
         #if username is not correct it won't do anything
@@ -138,14 +118,12 @@ class Software():
             self.uuid.configure(text=errorUUID)
             self.uuid.pack()
 
-             #configures the new skinPNG to the error image
+            #configures the new skinPNG to the error image
             self.skinPNG.configure(image=self.tk_error_image)
             self.skinPNG.pack()
 
-            #changes the UUID val to the error message
             self.uuid_val = errorUUID
             
-            #binds all of the changes
             self.root.bind("<Return>")
     
     #adds the UUID to the clipboard
@@ -162,28 +140,25 @@ class Software():
         except:
             pass
 
-        #############
         tk.mainloop()
 
 #handling the Mojang API requests
 class PlayerProperties():
     def __init__(self,username):
-        #GET the request from the Mojang API to get the player's uuid
+        #GET request to the Mojang API to get the player's uuid
         get_request = requests.get("https://api.mojang.com/users/profiles/minecraft/"+username)
-        
-        #load the Json request that was converted to text 
+         
         loaded_req = json.loads(get_request.text)
 
-        #get the player's uuid from the loaded request
+        #get the player's uuid from the loaded json request
         self.playerId = loaded_req["id"]
 
-        #GET the request from the Mojang API to get the skin of the player
+        #GET request to the Mojang API to get the skin of the player
         get_skin = requests.get("https://sessionserver.mojang.com/session/minecraft/profile/"+str(self.playerId))
 
-        #load the Json request that was converted to text
         loaded_get_skin = json.loads(get_skin.text)
 
-        #get the value from the loaded request, the value is in base64 so it also decodes it
+        #get the value from the loaded request and decodes it from base64
         decoded_props = base64.b64decode(loaded_get_skin["properties"][0]["value"])
 
         #load the decoded value
@@ -193,16 +168,13 @@ class PlayerProperties():
         self.skin_url = json_new_props["textures"]["SKIN"]["url"]
 
 if __name__ == '__main__':
-    #if there's no connection errors
     try:
         Software()
-    #if there's a connection error
+    #if there's a connection error:
     except:
-        #prints this to the console
-        print("Error: Check your network connection OR check if you're connected behind a firewall.\n") #2 new lines
+        #prints this to the console and not to a tkinter gui
+        print("Error: Check your network connection OR check if you're connected behind a firewall.", end="\n\n")
         print("The Software will close soon")
-
-        #5 seconds and the software closes
+        
         sleep(5)
-
         quit()
